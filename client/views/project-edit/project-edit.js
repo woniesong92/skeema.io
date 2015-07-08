959de51edb63d31347edec5d5b6fa5a90e875da6
sweetticket <yk494@cornell.edu>
2015-07-07 22:19:32 -0700

    toolbox helpers

--- client/views/project-edit/project-edit.js
+++ client/views/project-edit/project-edit.js
@@ -3,6 +3,27 @@ if (Meteor.isClient) {
   Template.ProjectEdit.helpers({
     project: function() {
       return this;
+    },
+    isBlockView: function () {
+      return Session.get("currentView") == "blockView";
+    },
+    isTrialView: function () {
+      return Session.get("currentView") == "trialView";
+    },
+    isFrameView: function () {
+      return Session.get("currentView") == "frameView";
+    },
+    block: function() {
+      var blockId = Session.get("id");
+      return Blocks.findOne({_id: blockId});
+    },
+    trial: function() {
+      var trialId = Session.get("id");
+      return Trials.findOne({_id: trialId});
+    },
+    frame: function() {
+      var frameId = Session.get("id");
+      return Frames.findOne({_id: frameId});
     }
   });
 
