26b3e7884499669fc42aa454b925e2a1f91063b4
sweetticket <yk494@cornell.edu>
2015-07-07 23:20:46 -0700

    global helpers

--- client/views/global/helpers.js
+++ client/views/global/helpers.js
@@ -2,40 +2,37 @@
 
 if (Meteor.isClient) {
 
-  Template.registerHelper("courseSelected", function(event, course) {
-    Router.go('/courses/'+course.catalog);
+  Template.registerHelper("isBlockView", function(event, course) {
+    return Session.get("currentView") == "blockView";
   });
 
-  Template.registerHelper("emailAddress", function(userId) {
-    if (userId == Meteor.user()._id) {
-      return Meteor.user().emails[0].address;
-    }
+  Template.registerHelper("isTrialView", function(event, course) {
+    return Session.get("currentView") == "trialView";
+  });
 
-    // needs publish/subscribe to fetch the user data
-    var user = Meteor.users.find({"_id":userId});
-    if (user) {
-      return user.emails[0].address;
-    } else {
-      return "n/a";
-    }
+  Template.registerHelper("isFrameView", function(event, course) {
+    return Session.get("currentView") == "frameView";
+  });
+
+  Template.registerHelper("block", function(event, course) {
+    var blockId = Session.get("id");
+    return Blocks.findOne({_id: blockId});
+  });
+
+  Template.registerHelper("trial", function(event, course) {
+    var trialId = Session.get("id");
+    return Trials.findOne({_id: trialId});
+  });
+
+  Template.registerHelper("frame", function(event, course) {
+    var frameId = Session.get("id");
+    return Frames.findOne({_id: frameId});
   });
   
-  //Template.registerHelper("isVerified", function() {
-  //  return (Meteor.userId() && Meteor.user().emails[0].verified);
-  //});
-  
 
-  Template.registerHelper("getCourses", function() {
-    return CourseSearch.getData({
-      transform: function(matchText, regExp) {
-        return matchText.replace(regExp, "<b>$&</b>")
-      }
-    });
-  });
-
-  Template.registerHelper("isLoading", function() {
-    return CourseSearch.getStatus().loading;
-  });
+  // Template.registerHelper("isLoading", function() {
+  //   return CourseSearch.getStatus().loading;
+  // });
 
   Template.registerHelper('isAdmin', function() {
     return (Meteor.user() && Meteor.user().role == "admin");
