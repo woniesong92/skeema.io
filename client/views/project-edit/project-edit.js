ff772ef2e1d3c460856e050f89ced2b1e0060ed1
sweetticket <yk494@cornell.edu>
2015-07-07 22:29:19 -0700

    comment convention

--- client/views/project-edit/project-edit.js
+++ client/views/project-edit/project-edit.js
@@ -28,9 +28,12 @@ if (Meteor.isClient) {
   });
 
   Template.ProjectEdit.rendered = function () {
+
+    // FIXME (LATER): NEED TO MAKE REACTIVE ONLY IN TEMPLATE (NOT GLOBAL); USE REACTIVE-VAR PACKAGE
     Session.set("currentView", "projectView");
-    Session.set("id", UI.getData()._id); // id of selected block, trial, or frame
-    debugger
+
+    // id of selected block, trial, or frame
+    Session.set("id", UI.getData()._id);
   }
 
   Template.ProjectEdit.events({
