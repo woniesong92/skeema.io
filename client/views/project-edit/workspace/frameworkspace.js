3a8373c17d94298099fe89c063d5b8f3a73ec7fe
sweetticket <yk494@cornell.edu>
2015-07-07 22:23:42 -0700

    workspace helpers

---
+++ client/views/project-edit/workspace/frameworkspace.js
@@ -0,0 +1,18 @@
+if (Meteor.isClient) {
+
+  Template.FrameWorkSpace.helpers({
+    // projects: function() {
+    //   return UI.getData();
+    // }
+  });
+
+  Template.FrameWorkSpace.rendered = function () {
+    
+  }
+
+  Template.FrameWorkSpace.events({
+  //   "click .project": function (e, template) {
+  //     
+  //   },
+  });
+}
