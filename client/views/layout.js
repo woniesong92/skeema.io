be4ebfd0c4b15052282fb27cdd2515e4cf19e1e0
sweetticket <yk494@cornell.edu>
2015-07-08 23:19:16 -0700

    smaller header

--- client/views/layout.js
+++ client/views/layout.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
     // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        var contentHeight = $(window).height() - 80;
+        var contentHeight = $(window).height() - 50;
 	    $('body').css("height", contentHeight);
   }
 }
