b798f7b2a7b0024163ca2c03789f8fc8fc42385a
woniesong92 <hs459@cornell.edu>
2015-07-13 13:52:11 -0700

    Cleaned up code a little and added necessary comments

--- client/views/project-edit/workspace/trialworkspace.js
+++ client/views/project-edit/workspace/trialworkspace.js
@@ -1,19 +1,100 @@
-function singleJSPWindow (frameId) {
+// TODO: these functions might fit better in Meteor.isClient()
+// because they are only used from the client side
 
+function makeFramesConnectable() {
+  jsPlumb.ready(function () {
+    var instance = jsPlumb.getInstance({
+      Endpoint: ["Dot", {radius: 2}],
+      HoverPaintStyle: {strokeStyle: "#1e8151", lineWidth: 2 },
+      ConnectionOverlays: [
+        [ "Arrow", {
+          location: 1,
+          id: "arrow",
+          length: 14,
+          foldback: 0.8
+        } ],
+        [ "Label", {
+          // label: "FOO",
+          id: "label",
+          cssClass: "aLabel"
+        }]
+      ],
+      Container: "frame-items-container"
+    });
+
+    // FIXME: is this okay to tie this instance val to template?
+    Template.TrialWorkSpace.jsp = instance;
+
+    var frames = jsPlumb.getSelector(".frame-preview-item");
+    instance.draggable(frames);
+
+    // bind a click listener to each connection; the connection is deleted. you could of course
+    // just do this: jsPlumb.bind("click", jsPlumb.detach), but I wanted to make it clear what was
+    // happening.
+    instance.bind("click", function (c) {
+      instance.detach(c);
+    });
+
+    // bind a connection listener. note that the parameter passed to this function contains more than
+    // just the new connection - see the documentation for a full list of what is included in 'info'.
+    // this listener sets the connection's internal
+    // id as the label overlay's text.
+    instance.bind("connection", function (info) {
+      info.connection.getOverlay("label").setLabel(info.connection.id);
+    });
+
+    // suspend drawing and initialise.
+    instance.batch(function () {
+      instance.makeSource(frames, {
+        filter: ".ep",
+        anchor: "Continuous",
+        connector: [ "StateMachine", { curviness: 20 } ],
+        connectorStyle: { strokeStyle: "#5c96bc", lineWidth: 2, outlineColor: "transparent", outlineWidth: 4 },
+        maxConnections: 10,
+        onMaxConnections: function (info, e) {
+          alert("Maximum connections (" + info.maxConnections + ") reached");
+        }
+      });
+
+      // initialise all '.frame-preview-item' elements as connection targets.
+      instance.makeTarget(frames, {
+        dropOptions: { hoverClass: "dragHover" },
+        anchor: "Continuous",
+        allowLoopback: true
+      });
+
+      // TODO: Paths will be saved in DB. Retrieve them and
+      // render(i.e. connect) them accordingly
+      // instance.connect({ source: "opened", target: "phone1" });
+    });
+
+    // Not really sure where I should need this event
+    jsPlumb.fire("jsPlumbDemoLoaded", instance);
+  });
+}
+
+
+/*
+  Same as makeFramesConnectable, but only for a single
+  frame
+*/
+
+function makeNewFrameConnectable (frameId) {
   var jsp = Template.TrialWorkSpace.jsp;
   var templateInstance = Template.instance();
   var numFrames = templateInstance.$('.frame-preview-item').length;
   var frameElement = templateInstance.$("#frame-" + frameId);
 
   jsp.draggable(frameElement);
-  
-  jsp.bind("click", function (c) {
-    jsp.detach(c);
-  });
 
-  jsp.bind("connection", function (info) {
-    info.connection.getOverlay("label").setLabel(info.connection.id);
-  });
+  // these were already set
+  // jsp.bind("click", function (c) {
+  //   jsp.detach(c);
+  // });
+
+  // jsp.bind("connection", function (info) {
+  //   info.connection.getOverlay("label").setLabel(info.connection.id);
+  // });
 
   jsp.batch(function () {
     jsp.makeSource(frameElement, {
@@ -40,13 +121,12 @@ function singleJSPWindow (frameId) {
   });
 }
 
-
 /*
-  Since all jsPlumb elements should be 
-  absolutely positioned, I have to order them
-  in JS rather than in CSS
+  Align all frame elements in DOM (3 elements/row).
+  Since all jsPlumb elements should be absolutely positioned, they should
+  be orederd using JS instead of CSS.
 */
-function positionElements() {
+function alignElements() {
   var frames = $('.frame-preview-item');
   var numFrames = frames.length;
   var containerWidth = $('.trial-workspace-container').width();
@@ -63,90 +143,6 @@ function positionElements() {
       top: topMargin
     });
   });
-
-  // place add item box in the last position
-  var lastLeftMargin = ((numFrames % 3) * space) + 50;
-  var lastTopMargin = (Math.floor(numFrames / 3) * 180) + topMargin;
-  $('.add-frame-item').css({
-    left: lastLeftMargin,
-    top: lastTopMargin
-  });
-}
-
-function setupJSPWindows() {
-  // jsPlumb helps users mark frames' connections
-  jsPlumb.ready(function () {
-
-    // setup some defaults for jsPlumb.
-    // var instance = jsPlumb.getInstance({
-    var instance = jsPlumb.getInstance({
-      Endpoint: ["Dot", {radius: 2}],
-      HoverPaintStyle: {strokeStyle: "#1e8151", lineWidth: 2 },
-      ConnectionOverlays: [
-        [ "Arrow", {
-          location: 1,
-          id: "arrow",
-          length: 14,
-          foldback: 0.8
-        } ],
-        [ "Label", {
-          // label: "FOO",
-          id: "label",
-          cssClass: "aLabel"
-        }]
-      ],
-      Container: "frame-items-container"
-    });
-
-    Template.TrialWorkSpace.jsp = instance;
-
-    var windows = jsPlumb.getSelector(".frame-preview-item");
-
-    // initialise draggable elements.
-    instance.draggable(windows);
-
-    // bind a click listener to each connection; the connection is deleted. you could of course
-    // just do this: jsPlumb.bind("click", jsPlumb.detach), but I wanted to make it clear what was
-    // happening.
-    instance.bind("click", function (c) {
-      instance.detach(c);
-    });
-
-    // bind a connection listener. note that the parameter passed to this function contains more than
-    // just the new connection - see the documentation for a full list of what is included in 'info'.
-    // this listener sets the connection's internal
-    // id as the label overlay's text.
-    instance.bind("connection", function (info) {
-      info.connection.getOverlay("label").setLabel(info.connection.id);
-    });
-
-    // suspend drawing and initialise.
-    instance.batch(function () {
-      instance.makeSource(windows, {
-        filter: ".ep",
-        anchor: "Continuous",
-        connector: [ "StateMachine", { curviness: 20 } ],
-        connectorStyle: { strokeStyle: "#5c96bc", lineWidth: 2, outlineColor: "transparent", outlineWidth: 4 },
-        maxConnections: 10,
-        onMaxConnections: function (info, e) {
-          alert("Maximum connections (" + info.maxConnections + ") reached");
-        }
-      });
-
-      // initialise all '.frame-preview-item' elements as connection targets.
-      instance.makeTarget(windows, {
-        dropOptions: { hoverClass: "dragHover" },
-        anchor: "Continuous",
-        allowLoopback: true
-      });
-
-      // and finally, make a couple of connections
-      // TODO: iterate over the path collection and connect the nodes accordingly
-      // instance.connect({ source: "opened", target: "phone1" });
-    });
-
-    jsPlumb.fire("jsPlumbDemoLoaded", instance);
-  });
 }
 
 if (Meteor.isClient) {
@@ -159,6 +155,7 @@ if (Meteor.isClient) {
       }
 
       // FIXME: there might be a way to replace Blaze.render and Blaze.remove
+      // Using Meteor.defer() seems simpler but its performance is worse
       frameItemsTemplate = Blaze.render(Template.FrameItems,
         $('.frame-items-container')[0]);
     });
@@ -172,14 +169,13 @@ if (Meteor.isClient) {
   });
 
   Template.FrameItems.onRendered(function() {
-
-    positionElements();
-    setupJSPWindows();
+    alignElements();
+    makeFramesConnectable();
 
     this.autorun(function() {
       var frameId = Session.get("frameAdded");
       if (frameId) {
-        singleJSPWindow(frameId);  
+        makeNewFrameConnectable(frameId);  
       }
     });
   });
