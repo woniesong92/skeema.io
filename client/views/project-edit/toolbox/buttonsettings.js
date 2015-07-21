46e317beb87faaee82cdcdee526ae137135cc9bf
Jenny Kim <yk494@cornell.edu>
2015-07-20 23:08:03 -0400

    button size

--- client/views/project-edit/toolbox/buttonsettings.js
+++ client/views/project-edit/toolbox/buttonsettings.js
@@ -29,11 +29,21 @@ if (Meteor.isClient) {
       var elementId = Session.get("elementId");
       $('#' + elementId).css('color', newcolor + ' !important');
     },
+
     'change.color #button-bg-colorpicker': function(e, template) { 
       debugger
       var newcolor = $('#button-bg-colorpicker').val().trim();
       var elementId = Session.get("elementId");
       $('#' + elementId).css('background-color', newcolor + ' !important');
+    },
+
+    'change #button-fontsize': function (e, template) { 
+      var newsize = $('#button-fontsize').val().trim();
+      var newheight = parseFloat(newsize) * 2;
+      var newlineheight = newheight;
+      var elementId = Session.get("elementId");
+      $('#' + elementId).css('font-size', newsize + 'px')
+        .css('height', newheight + 'px').css('line-height', newlineheight + 'px');
     },
 
   });
