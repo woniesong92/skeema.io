194444379a54d755f80e9cf9576bd4cdfa485f41
Jenny Kim <yk494@cornell.edu>
2015-07-25 11:23:43 -0400

    js injection

--- client/views/published/publishedtrial.js
+++ client/views/published/publishedtrial.js
@@ -2,8 +2,8 @@ if (Meteor.isClient) {
 
   Template.PublishedTrial.helpers({
     frames: function() {
-      return Frames.find();
-    }
+      return Frames.find({type: "normal"});
+    },
   });
 
 
@@ -25,8 +25,68 @@ if (Meteor.isClient) {
 
 
   Template.PublishedFrame.onRendered(function() {
+
+    var frameDOM = this.$('.frame-container');
+
     console.log("frame rendered");
-    // debugger
+
+    var isExit = this.data.type === "exit";
+
+    if (isExit){
+
+      //FIXME: when index is at end --> next block
+      var trial = Trials.findOne({_id: this.data.trialId});
+      var nextTrialId = Trials.findOne({index: trial.index + 1})._id;
+      Router.go('/preview/'+trial.blockId+'/'+nextTrialId);
+    }
+
+
+    var paths = Paths.find({sourceId: this.data._id}).fetch();
+
+    _.each(paths, function (path) {
+      var targetId = path.targetId;
+      var targetIsExit = Frames.findOne({_id: targetId}).type === "exit";
+
+
+      var scriptStr = "<script>" +
+        "var stepFrame = function() {" +
+          "if(targetIsExit){" +
+            "frameDOM.hide();" +
+            "$('.frame-container[data-framdId="'+ targetId +'"]').show();" +
+          "}" +
+        "}"+
+
+        "switch (path.eventType){" +
+          'case "keypress":' +
+            '$("document").keypress(function(e) {' +
+              'var code = e.keyCode || e.which;' +
+                'if (code === path.eventParam.charCodeAt(0)) {' +
+                  'stepFrame();' +
+                '}' +
+              '});' +
+            'break;' +
+
+          'case "time":' +
+
+            'setTimeout(function() {' +
+              'stepFrame();' +
+            '}, parseInt(path.eventParam));' +
+            'break;' +
+
+          'case "click":' +
+
+            "$('#' + path.eventParam).click(function() {" +
+              "stepFrame();" +
+            '});' +
+            'break;' +
+        '}' +
+      "</script>";
+
+      frameDOM.append(scriptStr);
+      debugger
+
+    });
+
   });
 
   Template.PublishedFrame.events({
