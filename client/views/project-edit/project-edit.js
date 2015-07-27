55da81b3ecd377ab357bd23c1e69acced2d262f7
Jenny Kim <yk494@cornell.edu>
2015-07-27 13:51:17 -0400

    first trial first block view, but need to open in sidenav

--- client/views/project-edit/project-edit.js
+++ client/views/project-edit/project-edit.js
@@ -1,32 +1,29 @@
 if (Meteor.isClient) {
   Template.ProjectEdit.onRendered(function() {
+    Session.set("currentView", "trialView");
     var projectId = this.data._id;
     var firstBlock = Blocks.findOne({
       projectId: projectId,
       index: 0
     });
+
+    var firstTrial = Trials.findOne({
+      projectId: projectId,
+      blockId: firstBlock._id,
+      index: 0
+    });
+
     Session.set("blockId", firstBlock._id);
+    Session.set("trialId", firstTrial._id);
+    Session.set("frameId", null);
+    Session.set("pathId", null);
+    Session.set("elementId", null);
   });
 
   Template.ProjectEdit.helpers({
     project: function() {
       return this;
     },
-  });
-
-  Template.ProjectEdit.onRendered(function() {
-
-    // FIXME (LATER): NEED TO MAKE REACTIVE ONLY IN TEMPLATE (NOT GLOBAL); USE REACTIVE-VAR PACKAGE
-    Session.set("currentView", "projectView");
-
-    Session.set("projectId", UI.getData()._id);
-
-    //should these be initialized?
-    Session.set("blockId", null);
-    Session.set("trialId", null);
-    Session.set("frameId", null);
-    Session.set("pathId", null);
-    Session.set("elementId", null);
   });
 
   Template.ProjectEdit.events({
