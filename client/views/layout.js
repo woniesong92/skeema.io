e1ea269e1ac5e2f0d6a68d71bced064fdc61b1fd
woniesong92 <hs459@cornell.edu>
2015-07-27 22:31:26 -0700

    fix indentation problems

--- client/views/layout.js
+++ client/views/layout.js
@@ -1,12 +1,7 @@
 if (Meteor.isClient) {  
 
   Template.Layout.rendered = function() {
-    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        var contentHeight = $(window).height() - 50;
-	    $('body').css("height", contentHeight);
+    var contentHeight = $(window).height() - 50;
+	  $('body').css("height", contentHeight);
   }
 }
